84c97e0968cb883d37556c9ca406d34ce7a70449
Igor Fernandes <palusterra@gmail.com>
2023-03-03 12:46:29 -0300

    Update rvet.c

--- rvet.c
+++ rvet.c
@@ -26,11 +26,31 @@ void Event(int pid, Clock *clock){
 
 void Send(int pid, Clock *clock){
    // TO DO
+ MPI_Status status;
+   int rank;
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   
+   // Atualiza o relógio local
+   clock->p[rank]++;
+   
+   // Envia mensagem
+   MPI_Send(&clock, sizeof(Clock), MPI_BYTE, pid_destino, 0, MPI_COMM_WORLD);
 }
 
 void Receive(int pid, Clock *clock){
    // TO DO
-
+ MPI_Status status;
+   int rank;
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   
+   // Recebe mensagem
+   MPI_Recv(clock, sizeof(Clock), MPI_BYTE, pid_origem, 0, MPI_COMM_WORLD, &status);
+   
+   // Atualiza o relógio local
+   clock->p[rank]++;
+   
+   // Atualiza o relógio do processo que enviou a mensagem
+   clock->p[pid_origem]++;
 }
 
 // Representa o processo de rank 0
@@ -40,6 +60,14 @@ void process0(){
    printf("Process: %d, Clock: (%d, %d, %d)\n", 0, clock.p[0], clock.p[1], clock.p[2]);
 
    // TO DO
+ // Envia mensagem para processo 1
+   Send(1, &clock);
+   
+   // Recebe mensagem de processo 2
+   Receive(2, &clock);
+   
+   // Imprime relógio final
+   printf("Process: %d, Clock: (%d, %d, %d)\n", 0, clock.p[0], clock.p[1], clock.p[2]);
 
 }
 
@@ -49,6 +77,17 @@ void process1(){
    printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 
    // TO DO
+ // Recebe mensagem de processo 0
+   Receive(0, &clock);
+   
+   // Atualiza relógio local
+   Event(1, &clock);
+   
+   // Envia mensagem para processo 2
+   Send(2, &clock);
+   
+   // Imprime relógio final
+   printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 }
 
 // Representa o processo de rank 2
@@ -57,6 +96,17 @@ void process2(){
    printf("Process: %d, Clock: (%d, %d, %d)\n", 2, clock.p[0], clock.p[1], clock.p[2]);
    
    // TO DO
+ // Envia mensagem para processo 0
+   Send(0, &clock);
+   
+   // Recebe mensagem de processo 1
+	Receive(1, &clock);
+
+	// Atualiza relógio local
+	Event(2, &clock);
+
+	// Imprime relógio final
+	printf("Process: %d, Clock: (%d, %d, %d)\n", 2, clock.p[0], clock.p[1], clock.p[2]);
 }
 
 int main(void) {
